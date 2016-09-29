c7ca4f50d18461c4b61758656226eb290cb8d066
ashes999 <ashes999@yahoo.com>
2016-09-28 22:49:44 -0400

    Recreate prototype: images

--- source/Sources/CoreGame.hx
+++ source/Sources/CoreGame.hx
@@ -14,7 +14,9 @@ class CoreGame {
   	public static inline var screenHeight = 450;
 
 	private var backbuffer:Image;
-	private var background:Image;
+
+	private var images:Map<String, Image> = new Map<String, Image>();
+
 	private var initialized:Bool = false;
 
 	public function new()
@@ -23,10 +25,14 @@ class CoreGame {
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 
 		backbuffer = Image.createRenderTarget(screenWidth, screenHeight);
+
 		Assets.loadEverything(function()
 		{			
 			initialized = true;
-			background = Assets.images.background;
+			images["background"] = Assets.images.background;
+			images["player"] = Assets.images.player;
+			images["scenery"] = Assets.images.scenery;
+			images["monster"] = Assets.images.monster;
 		});
 	}
 
@@ -46,7 +52,10 @@ class CoreGame {
 
 		// clear our backbuffer using graphics2
 		g.begin(true, Color.Black);
-		g.drawImage(background, 0, 0);
+		g.drawImage(images["background"], 0, 0);
+		g.drawImage(images["scenery"], 25, 75);
+		g.drawImage(images["player"], 150, 150);
+		g.drawImage(images["monster"], 550, 150);
 		g.end();
 
 		// draw our backbuffer onto the active framebuffer
